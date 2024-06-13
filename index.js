c14e01e6d15fe8c723391f1fb13d8277199a252b
Madjid <djerada.madjid@gmail.com>
2024-06-13 17:59:00 +0200

    Listning port handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require ("node:http")
 const server = http.createServer((req,res)=>{
     return res.writeHead(200,{"Content-Type":"text/html"}).end("My test with Barghod")
 })
-server.listen(4000, ()=>{
-    console.log("Server listen on port 4000")
+const port = process.env.PORT || 4000
+server.listen(port, ()=>{
+    console.log(`Server listen on port ${port}`)
 })
